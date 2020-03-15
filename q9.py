6baf799709481350d827a8fb0a16ef4ebde2b8c2
Nicholas <52936504+z-dot@users.noreply.github.com>
2020-03-15 10:15:39 +0000

    Create q9.py

---
+++ q9.py
@@ -0,0 +1,68 @@
+from typing import Optional, List, Tuple, Set, Union
+
+class Node:
+    MAX_DEPTH = 15
+    all_grids = set()
+
+    def __init__(self, trees: Union[set, frozenset] = {(0,0),}, depth=0, parent=None):
+        self.parent = parent
+        self.depth = depth
+        self.trees = frozenset(trees)
+        if self.depth != Node.MAX_DEPTH and self.trees not in Node.all_grids:
+            self.children = self.make_children()
+            Node.all_grids.add(self.trees)
+        else:
+            self.children = []
+
+    def get_options(self) -> List[Tuple[int, int]]:
+        output = []
+        for tree in self.trees:
+            if (tree[0] + 1, tree[1]) not in self.trees and (
+                tree[0],
+                tree[1] + 1,
+            ) not in self.trees:
+                output.append(tree)
+        return output
+
+    def make_children(self) -> List["Node"]:
+        children = []
+        for tree in self.get_options():
+            temp_set = set(self.trees)
+            temp_set.remove(tree)
+            temp_set.add((tree[0] + 1, tree[1]))
+            temp_set.add((tree[0], tree[1] + 1))
+            children.append(Node(temp_set, self.depth + 1, parent=self))
+        return children
+
+
+def count_children(a_node: Node) -> int:
+    return len(a_node.children) + sum(
+        [count_children(child) for child in a_node.children]
+    )
+
+
+def find_diagonals(a_node: Node, k) -> Optional[Node]:
+    for tree in a_node.trees:
+        if tree[0] + tree[1] + 1 <= k:
+            break
+    else:
+        return a_node
+    for child in a_node.children:
+        if (c := find_diagonals(child, k)):
+            return c
+    return None
+
+def count_distinct(a_node: Node) -> Set[frozenset]:
+    master_set = set()
+    master_set.add(a_node.trees)
+    for child in a_node.children:
+        master_set |= count_distinct(child)
+    return master_set
+
+
+if __name__ == "__main__":
+    q = Node()
+    print(f"Distinct tree grids up to depth {Node.MAX_DEPTH}: {len(Node.all_grids)}")
+    for k in range(1, 10):
+        print(f"Is there a grid where the first {k} diagonals are empty: {bool(find_diagonals(q, k))}")
+    
